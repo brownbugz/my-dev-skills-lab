489129c16f8528a7400d5d30e740c882a3cf69ac
sonniakemmer <brownbugz@gmail.com>
2019-08-14 21:50:04 -0700

    add flights and views index

---
+++ mongoose/mongoose-flights/models/flight.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            return new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+        }
+    }
+});
+
+var Flight = mongoose.model('Flight', flightSchema);
+
+module.exports = Flight;
