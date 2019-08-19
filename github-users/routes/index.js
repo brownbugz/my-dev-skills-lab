1d8b6941598387c19c95f2f9e45e2e34e686cddd
sonniakemmer <brownbugz@gmail.com>
2019-08-19 13:25:30 -0700

    add username

--- github-users/routes/index.js
+++ github-users/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+  console.log(`username: ${req.body.username}`);
+  res.render('index');
 });
 
 module.exports = router;
